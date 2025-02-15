7a488b45720af455ae3efd52e3c559b378a4256c
Jisoo.R <jisuryu0527@gmail.com>
2025-02-16 01:28:23 +0900

    feat(sign-dto, repo): 로그인 로직용 dto 추가, 검증용 로직 타입 변환 #5

--- src/main/java/com/example/onboarding_backend_java/repository/UserRepository.java
+++ src/main/java/com/example/onboarding_backend_java/repository/UserRepository.java
@@ -4,10 +4,12 @@ import com.example.onboarding_backend_java.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
     Boolean existsByUsername(String username);
 }
