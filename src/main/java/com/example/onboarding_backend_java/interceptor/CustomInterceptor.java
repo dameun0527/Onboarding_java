b033578bb97df4e3a7a456600c9d43b09fc5fd47
Jisoo.R <jisuryu0527@gmail.com>
2025-02-16 15:05:27 +0900

    feat(logging-interceptor): Interceptor 기능 추가 #9 - preHandle() - postHandle() - afterCompletion()

---
+++ src/main/java/com/example/onboarding_backend_java/interceptor/CustomInterceptor.java
@@ -0,0 +1,29 @@
+package com.example.onboarding_backend_java.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Component
+public class CustomInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("[CustomInterceptor] preHandle - URI: {}", request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("[CustomInterceptor] postHandle - URI: {}", request.getRequestURI());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("[CustomInterceptor] afterCompletion - URI: {}", request.getRequestURI());
+    }
+}
