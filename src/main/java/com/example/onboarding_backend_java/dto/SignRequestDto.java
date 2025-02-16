3da687314f2e9a709e52cb03682735f7c0d3c170
Jisoo.R <jisuryu0527@gmail.com>
2025-02-16 17:41:37 +0900

    feat(sign-dto): 로그인 API 분리를 위한 dto 추가 #5

--- src/main/java/com/example/onboarding_backend_java/dto/SignRequestDto.java
+++ src/main/java/com/example/onboarding_backend_java/dto/SignRequestDto.java
@@ -1,13 +1,7 @@
 package com.example.onboarding_backend_java.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@Getter
-@AllArgsConstructor
-public class SignRequestDto {
-    private String username;
-    private String password;
+public record SignRequestDto (
+        String username,
+        String password
+) {
 }
---
+++ src/main/java/com/example/onboarding_backend_java/dto/SignResponseDto.java
@@ -0,0 +1,6 @@
+package com.example.onboarding_backend_java.dto;
+
+public record SignResponseDto(
+        String token
+) {
+}
